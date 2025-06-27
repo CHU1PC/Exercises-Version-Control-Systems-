9173f93208adc096003d662c3a7253ec9b90d7e5
CHU1PC <167416194+CHU1PC@users.noreply.github.com>
2025-06-27 01:08:09 +0000

    Won/lost message modified

--- dice.py
+++ dice.py
@@ -1,22 +1,23 @@
 import numpy as np
 
 
-def roll_dice():
+def roll_dice(name):
     print("Rolling dice...")
     a, b = np.random.randint(1, 7, 2)
     print("Die 1:", a)
     print("Die 2:", b)
     print("Total value:", a + b)
     if a + b >= 7:
-        print("You won!")
+        print(f"{name} won!")
     else:
-        print("You lost.")
+        print(f"{name} lost.")
 
 
 def greeting():
     name = input("What is your name?\n")
     print(f"Hello, {name}!")
+    return name
 
 if __name__ == "__main__":
-    greeting()
-    roll_dice()
+    name = greeting()
+    roll_dice(name)
